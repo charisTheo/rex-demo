1688ec5a1ecac6d8cbb3ab01e934501205de9204
Harry Theo <harrytheo@google.com>
2023-08-15 10:28:20 +0100

    Fixed API root route.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
 const router = new express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('/server/public/index.html', {title: 'Express', root: '.'});
+  res.send('Rex API Server 🐕');
 });
 
 export default router;
