8c25a305628a3342827626e2ae3e92fa2fd20ddb
Harry Theo <harrytheo@google.com>
2023-08-17 14:28:01 +0100

    Linter fixes.

--- server/controllers/puppeteer.js
+++ server/controllers/puppeteer.js
@@ -3,61 +3,63 @@ import log from '../utils/log.js';
 const {randomUUID} = await import('node:crypto');
 
 /**
- * 
- * @param {*} config 
- * @returns 
+ *
+ * @param {*} config
+ * @return {String|undefined}
  * Given a config file this function will perform a simple tracing based on the config
  */
 async function tracing(config) {
-    const {
-        pageUrl,
-        emulateCPUThrottling,
-        slow3G,
-        viewportConfig,
-    } = (config || {});
-    
-    const traceFileName = `${randomUUID()}.json`;
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({headless: 'new'});
-    const page = await browser.newPage();
-    // Set screen size
-    if (viewportConfig) {
-        await page.setViewport(viewportConfig); 
-    }
-    // Set network condition
-    if (slow3G) {
-       await page.emulateNetworkConditions(PredefinedNetworkConditions['Slow 3G']);
-    }
-    //set CPU condition
-    if (emulateCPUThrottling) {
-        await page.emulateCPUThrottling(4);
-    }
-    try {
-        // Configure the navigation timeout
-        await page.setDefaultNavigationTimeout(0);
-        //start tracing 
-        await page.tracing.start({path: 'traces/' + traceFileName});
-        // Navigate the page to a URL
-        await page.goto(pageUrl);
-        await page.tracing.stop();
-        await browser.close();
-        return traceFileName;
-    } catch(error) {
-        log(error)
-        return undefined;
-    }
+  const {
+    pageUrl,
+    emulateCPUThrottling,
+    slow3G,
+    viewportConfig,
+  } = (config || {});
+
+  const traceFileName = `${randomUUID()}.json`;
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({headless: 'new'});
+  const page = await browser.newPage();
+  // Set screen size
+  if (viewportConfig) {
+    await page.setViewport(viewportConfig);
+  }
+  // Set network condition
+  if (slow3G) {
+    await page.emulateNetworkConditions(PredefinedNetworkConditions['Slow 3G']);
+  }
+  // set CPU condition
+  if (emulateCPUThrottling) {
+    await page.emulateCPUThrottling(4);
+  }
+  try {
+    // Configure the navigation timeout
+    await page.setDefaultNavigationTimeout(0);
+    // start tracing
+    await page.tracing.start({path: 'traces/' + traceFileName});
+    // Navigate the page to a URL
+    await page.goto(pageUrl);
+    await page.tracing.stop();
+    await browser.close();
+    return traceFileName;
+  } catch (error) {
+    log(error);
+    return undefined;
+  }
 }
 
 /**
- * 
- * @param {*} pageUrl 
- * Given a page Url this function will run tracing experience 
+ *
+ * @param {*} pageUrl
+ * @param {Boolean} emulateCPUThrottling
+ * @param {Boolean} slow3G
+ * @param {Object} viewportConfig
+ * Given a page Url this function will run tracing experience
  * then open the result in a tracing view
  */
-
 async function replayExperience(pageUrl, emulateCPUThrottling, slow3G, viewportConfig) {
-    const traceResultJson = await tracing({pageUrl, emulateCPUThrottling, slow3G, viewportConfig});
-    return traceResultJson || null;
+  const traceResultJson = await tracing({pageUrl, emulateCPUThrottling, slow3G, viewportConfig});
+  return traceResultJson || null;
 }
 
 export {replayExperience};
